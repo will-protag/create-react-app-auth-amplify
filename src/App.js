2c684288e4d69734b5e63bada1a4d005d5512728
will-protag <ec2-user@ip-172-31-42-3.us-west-2.compute.internal>
2021-12-02 03:45:44 +0000

    adding download link

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
+// import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 import { saveAs } from "file-saver";
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -21,6 +24,28 @@ class App extends Component {
       </div>
     );
   }
+}
+
+export function downloadBlob(blob, filename) {
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename || 'download';
+  const clickHandler = () => {
+    setTimeout(() => {
+      URL.revokeObjectURL(url);
+      a.removeEventListener('click', clickHandler);
+    }, 150);
+  };
+  a.addEventListener('click', clickHandler, false);
+  a.click();
+  return a;
+}
+
+// usage
+async function download() {
+  const result = await Storage.get(`Cyball-Android-debug.zip`, { download: true }, { contentType: "application/zip"});
+  downloadBlob(result.Body, 'filename');
 }
 
 export default withAuthenticator(App);
---
+++ src/test.js
@@ -0,0 +1,7 @@
+function sum(x, y) {
+  return x + y
+}
+
+let total = sum(10, 20)
+
+console.log(total);
