e4db3e98b150f8bec02ab36b3e770c06af742485
will-protag <ec2-user@ip-172-31-42-3.us-west-2.compute.internal>
2021-12-03 22:05:49 +0000

    adding buttons

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 // import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
-import { saveAs } from "file-saver";
 import Amplify, { Storage } from 'aws-amplify';
 import awsconfig from './aws-exports';
+import { Button, ButtonGroup } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 Amplify.configure(awsconfig);
 Amplify.configure(aws_exports);
 
@@ -22,6 +23,11 @@ class App extends Component {
           </p>
           {/* <button className="DefButton" onClick={ () => downloadWindows()}>Cyball Windows (zip)</button><br /> */}
           {/* <button className="DefButton" onClick={ () => downloadAndroid()}>Cyball Android (zip)</button> */}
+          <Button onClick={() => alert('👋 Welcome!')}>Click me</Button><br />
+          <ButtonGroup direction="column">
+          <Button>This is a test</Button>
+          <Button>This is also a test 🎉</Button>
+          </ButtonGroup>
         </header>
       </div>
     );
